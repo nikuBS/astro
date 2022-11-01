6bd9844d28dde88ff360615c6640829ac7b353e2
naniku <rizard000@gmail.com>
2022-11-01 13:24:47 +0900

    1차 개발 완료

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '김진영 y 명승희 결혼식';
+export const SITE_DESCRIPTION = '11/26 11시 더컨벤션 영등포점';
---
+++ src/type/string.ts
@@ -0,0 +1,18 @@
+export const WEDDING = {
+    HALL: '더컨벤션 영등포점 2층 다이너스티홀',
+    ADDRESS: '서울 영등포구 국회대로38길 2',
+    TIME: '202211261100'
+};
+
+export const NAVI_TYPE = {
+    T_MAP: {
+        scheme: 'https://surl.tmap.co.kr/7a039904'
+    },
+    NAVER_MAP: {
+        scheme: 'https://map.naver.com/v5/entry/place/1241035074'
+    },
+    KAKAO_MAP: {
+        scheme: 'https://map.kakao.com/link/search/'
+    }
+
+};
