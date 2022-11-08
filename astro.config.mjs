0b992468ee1c2c478ba518899a7f1c9a84d4416e
naniku <rizard000@gmail.com>
2022-11-08 10:13:09 +0900

    빌드 업로드

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://nikubs.github.io/astro',
+	site: 'https://nikubs.github.io/astro/',
 	integrations: [mdx(), sitemap()],
 });
--- docs/hoisted.e86ec3d8.js
+++
@@ -1,1 +0,0 @@
-import"./chunks/BaseHead.astro_astro_type_script_index_0_lang.e9230d01.js";const e=$("#date-picker").datepicker();e.datepicker("setDate","20221126");
