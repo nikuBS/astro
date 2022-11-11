354223a296d2032b5ea544bf38965f63cd3d8d76
naniku <rizard000@gmail.com>
2022-11-11 09:51:32 +0900

    base URL 빌드 시 자동 지정되도록 변경 OG.META 이미지 변경

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://nikubs.github.io/astro/',
+	site: 'https://nikubs.github.io',
+	base: '/astro',
 	integrations: [mdx(), sitemap()],
 });
